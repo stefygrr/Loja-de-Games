c08da2790f1231c79c1b26d4502723f2fea7a535
Stephany camily <stefycamly@gmail.com>
2023-04-27 17:31:20 -0300

    Atividade Security 27/04

---
+++ src/main/java/com/generation/lojinhagames/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.lojinhagames.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.lojinhagames.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
